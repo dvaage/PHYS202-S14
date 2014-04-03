b2a844b4c27b5be0162918d51ce222b74995f8f2
Devin Vaage <dvaage@caloly.edu>
2014-04-03 12:56:05 -0700

    adding config and pledge

---
+++ configureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Devin Vaage"
+git config --global user.email "dvaage@caloly.edu" 
+git config --global color.ui "auto"
+git config --global core.editor "nano" 
