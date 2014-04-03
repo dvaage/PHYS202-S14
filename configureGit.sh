6f133a35d754c6dbccc53c3ca0298c7fd75d1f7d
Devin Vaage <dvaage@caloly.edu>
2014-04-03 13:06:03 -0700

    added a documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# This script configures git whenever you start working 
+# on multiple machines. Run it anytime you diced to work 
+# with git on a new computer 
+
 git config --global user.name "Devin Vaage"
-git config --global user.email "dvaage@caloly.edu" 
+git config --global user.email "dvaage@calpoly.edu" 
 git config --global color.ui "auto"
 git config --global core.editor "nano" 
